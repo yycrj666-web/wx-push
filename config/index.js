1c264934b8c81f13e547ceb7187c4e1d08fa604a
友猿创软件 <yycrj666@gmail.com>
2025-08-25 05:05:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wxdebfef8bc53ec0d8",
   appsecret: "036a3752f3f8136d2f2c2289adaa3c36",
-  template_id: "xtfPdvpFDlgvfygoX-9rxPiIp5U59Ds7tfZiAHLuVzc",
+  template_id: "bAdQJHEM8xg7pnJsYylVwsbYJwSCp46CI03kQfdVE58",
   touser: "ou_QS2MbjuUCRNCyu8fJyzdhLTFQ",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
